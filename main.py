b50052709402de83ef53ab505f80b82958b5ef96
Davi <davimoreiraasantoss@gmail.com>
2026-03-04 21:56:23 -0300

    project complete

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import cleaning as clean
 import visualization as visu
 
 emendas = clean.carregar_e_limpar()
+
+visu.top_10_uf(emendas)
+visu.total_por_ano(emendas)
+visu.top_10_funcao(emendas)
+visu.empenhado_e_pago(emendas)
 visu.top_10_eficientes(emendas)
